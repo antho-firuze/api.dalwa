f6f0f9ceaac1f47ae077098a9e73ba121ed1c928
Ahmad <antho.firuze@gmail.com>
2019-08-08 18:11:57 +0700

    Update

--- __application/models/dalwa-api/Wali_model.php
+++ __application/models/dalwa-api/Wali_model.php
@@ -36,9 +36,9 @@ class Wali_model extends CI_Model
 			$this->db->select($request->params->fields);
 
 		$str = '(
-      select a.login_id, b.partner_id, a.reg_no, TRIM(CONCAT(b.first_name, " ", IFNULL(b.last_name,""))) as full_name
+      select a.login_id, b.partner_id, b.reg_no, TRIM(CONCAT(b.first_name, " ", IFNULL(b.last_name,""))) as full_name
 			from a_login_santri a
-			inner join c_partner b on a.reg_no = b.reg_no
+			inner join c_partner b on a.partner_id = b.partner_id
       where a.client_id = ? and a.login_id = ?
 		) g0';
 		$table = $this->f->compile_qry($str, [$request->client_id, $request->login_id]);
@@ -57,7 +57,7 @@ class Wali_model extends CI_Model
 		if (isset($request->params->fields) && !empty($request->params->fields))
 			$this->db->select($request->params->fields);
 		else 
-			$this->db->select('reg_no, TRIM(CONCAT(first_name, " ", IFNULL(last_name,""))) as full_name');
+			$this->db->select('partner_id, reg_no, TRIM(CONCAT(first_name, " ", IFNULL(last_name,""))) as full_name');
 
 		if (!$result = $this->db->get_where('c_partner', ['client_id' => $request->client_id, 'reg_no' => $request->params->nis]))
 			return [FALSE, ['message' => 'Database Error: '.$this->db->error()['message']]];		
@@ -65,13 +65,13 @@ class Wali_model extends CI_Model
 		if (!$row = $result->row()) 
 			return [FALSE, ['message' => $this->f->_err_msg('err_nis_not_found')]];
 		
-		if (!$result = $this->db->get_where('a_login_santri', ['client_id' => $request->client_id, 'login_id' => $request->login_id, 'reg_no' => $request->params->nis]))
+		if (!$result = $this->db->get_where('a_login_santri', ['client_id' => $request->client_id, 'login_id' => $request->login_id, 'partner_id' => $row->partner_id]))
 			return [FALSE, ['message' => 'Database Error: '.$this->db->error()['message']]];		
 
 		if ($row2 = $result->row()) 
 			return [FALSE, ['message' => $this->f->_err_msg('err_nis_duplicate')]];
 
-		if (!$result = $this->db->insert('a_login_santri', ['client_id' => $request->client_id, 'login_id' => $request->login_id, 'reg_no' => $row->reg_no]))
+		if (!$result = $this->db->insert('a_login_santri', ['client_id' => $request->client_id, 'login_id' => $request->login_id, 'partner_id' => $row->partner_id]))
 			return [FALSE, ['message' => 'Database Error: '.$this->db->error()['message']]];		
 
 		return [TRUE, ['message' => $this->f->lang('sucess_adding_santri'), 'result' => (object)['nis' => $row->reg_no, 'full_name' => $row->full_name]]];
